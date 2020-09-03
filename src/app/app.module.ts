08f5722ddc454496813246679703df5729e9e512
Quintaneishon <ajitzi.quintana@gmail.com>
2020-09-02 20:36:51 -0500

    listo el cascaron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AcademicaComponent } from './components/academica/academica.component';
+import { LaboralComponent } from './components/laboral/laboral.component';
+import { InteresesComponent } from './components/intereses/intereses.component';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    AcademicaComponent,
+    LaboralComponent,
+    InteresesComponent,
+    HobbiesComponent,
+    ContactoComponent,
+    ProyectosComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule
